a943b47f1d9666fe92a406ad2d2e382f95ab5857
Andrew_Simmons <andrew.b.simmons01@gmail.com>
2021-08-25 16:42:01 -0400

    create Badges Project

---
+++ Badges/Badge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badges
+{
+    class Badge
+    {
+        public string BadgeID { get; set; }
+        public List<string> DoorAccess { get;  }
+        public Badge (string badgeID, List<string> doorAccess)
+        {
+            BadgeID = badgeID;
+            DoorAccess = doorAccess;
+        }
+    }
+}
---
+++ Badges/BadgeUI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Badges
+{
+    class BadgeUI
+    {
+        public void Run()
+        {
+            MainMenu();
+        }
+        public void MainMenu()
+        {
+            Console.WriteLine("Admin Menu\n\n" +
+                "Hello Security Admin What Would You Like To Do?\n\n" +
+                "1.Add A Badge\n" +
+                "2.Edit A Badge\n" +
+                "3. List All Badges");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    AddABadge();
+                    break;
+                case "2":
+                    EditABadge();
+                    break;
+                case "3":
+                    ListAllBadges();
+                case "4":
+                    Exit();
+                    Console.Clear();
+                    Console.WriteLine("GoodBye!");
+                    Thread.Sleep(400);
+                    break;
+
+            }
+        }
+        public void AddABadge()
+        {
+
+        }
+        public void EditABAdge()
+        {
+
+        }
+        public void ListAllBadges()
+        {
+
+        }
+    }
+}
